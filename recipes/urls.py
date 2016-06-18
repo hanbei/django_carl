aed1bfc0b35d878db61b749f646c00cfa5f93fea
Florian Schulz <hanbei@hanbei.de>
2016-06-18 17:19:37 +0200

    Use generic views for list and detail

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.random_recipe, name='random'),
-    url(r'^recipes/$', views.index, name='index'),
-    url(r'^recipes/(?P<recipe_id>[0-9]+)/$', views.details, name='details'),
+    url(r'^recipes/$', views.ListView.as_view(), name='index'),
+    url(r'^recipes/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='details'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 import random
 
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from recipes.models import Recipe
 
 
@@ -10,13 +11,24 @@ def random_recipe(request):
     return render(request=request, template_name='recipes/random.html', context=context)
 
 
-def index(request):
-    recipes = Recipe.objects.order_by('id')
-    print(recipes)
-    context = {'recipes': recipes,}
-    return render(request=request, template_name='recipes/index.html', context=context)
+class ListView(generic.ListView):
+    template_name = 'recipes/index.html'
+    context_object_name = 'recipes'
+
+    def get_queryset(self):
+        return Recipe.objects.order_by('id')
 
 
-def details(request, recipe_id):
-    recipe = get_object_or_404(Recipe, pk=recipe_id)
-    return render(request=request, template_name='recipes/details.html', context={'recipe': recipe})
+# def index(request):
+#     recipes = Recipe.objects.order_by('id')
+#     print(recipes)
+#     context = {'recipes': recipes,}
+#     return render(request=request, template_name='recipes/index.html', context=context)
+
+class DetailView(generic.DetailView):
+    model = Recipe
+    template_name = 'recipes/details.html'
+
+# def details(request, recipe_id):
+#     recipe = get_object_or_404(Recipe, pk=recipe_id)
+#     return render(request=request, template_name='recipes/details.html', context={'recipe': recipe})
