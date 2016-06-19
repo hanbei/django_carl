b71950585f8bdfd870229509985f51d51122b1b6
Florian Schulz <hanbei@hanbei.de>
2016-06-19 20:47:27 +0200

    Alter recipe model and let django handle the id

--- recipes/models.py
+++ recipes/models.py
@@ -6,9 +6,8 @@ from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
 class Recipe(models.Model):
-    id = models.IntegerField(primary_key=True)
-    title = models.CharField(max_length=80, unique=True)
-    description = models.CharField(max_length=120, unique=True)
+    title = models.CharField(max_length=80)
+    description = models.CharField(max_length=120)
     rating = models.SmallIntegerField(default=0)
 
     def __str__(self):
